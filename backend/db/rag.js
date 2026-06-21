29c22046015473b05a1283bc480a627773b1e7ab
Radhe Chaudhary <radhechaudhary6398@gmail.com>
2026-06-21 22:35:58 +0530

    added online vector db

--- backend/db/rag.js
+++ backend/db/rag.js
@@ -1,6 +1,12 @@
-import { ChromaClient } from "chromadb";
+import { CloudClient } from "chromadb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const client = new ChromaClient();
+const client = new CloudClient({
+    apiKey: process.env.CHROMA_API_KEY,
+    tenant: process.env.CHROMA_TENANT,
+    database: process.env.CHROMA_DATABASE
+});
 
 const collection = await client.getOrCreateCollection({
     name: "meet-chunks",
--- backend/routes/upload-chunk.route.js
+++ backend/routes/upload-chunk.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { uploadChunk } from "../controllers/upload-chunk.controller.js";
-import { verifyToken } from "../middlewares/verifyToken.middleware.js";
+import { verifySession } from "../middlewares/verifySession.middleware.js";
 
 const router = Router();
 
-router.post("/upload-chunk", verifyToken, uploadChunk);
+router.post("/upload-chunk", verifySession, uploadChunk);
 
 export default router;
